8f3f1b803fe3af10a5a15254768ddb3cf53c957b
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-02-15 10:20:45 -0600

    Set up for native npm stop

--- bin/stopApp.js
+++ bin/stopApp.js
@@ -1,8 +1,8 @@
-const io = require('socket.io-client');
-const socketClient = io.connect('http://localhost'); // Specify port if your express server is not using default port 80
+const io2 = require('socket.io-client');
+const socketClient2 = io2.connect('http://localhost'); // Specify port if your express server is not using default port 80
 
-socketClient.on('connect', () => {
-  socketClient.emit('npmStop');
+socketClient2.on('connect', () => {
+  socketClient2.emit('npmStop');
   setTimeout(() => {
     process.exit(0);
   }, 1000);
