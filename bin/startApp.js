0a27ebcde12fed2f57f2ac7083337b1ed5d7bda1
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-02-16 13:44:49 -0600

    remove semicolons

--- bin/startApp.js
+++ bin/startApp.js
@@ -4,51 +4,51 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('rerum_server_nodejs:server');
-var http = require('http');
+var app = require('../app')
+var debug = require('debug')('rerum_server_nodejs:server')
+var http = require('http')
 
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
-const io = require('socket.io')(server);
+var server = http.createServer(app)
+const io = require('socket.io')(server)
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  const portCheck = parseInt(val, 10);
+  const portCheck = parseInt(val, 10)
 
   if (isNaN(portCheck)) {
     // named pipe
-    return val;
+    return val
   }
 
   if (portCheck >= 0) {
     // port number
-    return portCheck;
+    return portCheck
   }
 
-  return false;
+  return false
 }
 
 /**
@@ -57,25 +57,25 @@ function normalizePort(val) {
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
 
   var bind = typeof port === 'string'
     ? 'Pipe ' + port
-    : 'Port ' + port;
+    : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
 }
 
@@ -84,11 +84,11 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
+  var addr = server.address()
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+    : 'port ' + addr.port
+  debug('Listening on ' + bind)
 }
 
 /**
@@ -96,7 +96,7 @@ function onListening() {
  * */
 io.on('connection', (socketServer) => {
   socketServer.on('npmStop', () => {
-    process.exit(0);
-  });
-});
+    process.exit(0)
+  })
+})
 
--- db-controller.js
+++ db-controller.js
@@ -1,75 +1,71 @@
 #!/usr/bin/env node
 
-var mongoose = require('mongoose')
 const { MongoClient } = require('mongodb')
+var ObjectID = require('mongodb').ObjectID
 console.log("Controller is making a mongo connection...")
-const client = new MongoClient(process.env.ATLAS_CONNECTION_STRING2);
-client.connect();
-
-// Import contact model
-Model = require('./db-object-model.js');
+const client = mongoConnection()
 
 // Handle index actions
 exports.index = function (req, res) {
     res.json({
         status: "connected",
         message: "Not sure what to do"
-    });
-};
+    })
+}
 
 // Create object passed in the body
 // TODO only registered apps should be able to do this.  It needs to generate the __rerum property.
 exports.create = async function (req, res) {
-    res.set("Content-Type", "application/json; charset=utf-8");
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "*");
-    res.set("Access-Control-Expose-Headers", "*");
-    res.set("Access-Control-Allow-Methods", "*");
+    res.set("Content-Type", "application/json charset=utf-8")
+    res.set("Access-Control-Allow-Origin", "*")
+    res.set("Access-Control-Allow-Headers", "*")
+    res.set("Access-Control-Expose-Headers", "*")
+    res.set("Access-Control-Allow-Methods", "*")
     try{
-        const id = new mongoose.Types.ObjectId().toString();
-        let obj = req.body;
-        obj["_id"] = id;
-        obj["@id"] = "https://storedev.rerum.io/v1/id/"+id;
-        console.log("Creating an object (no history or __rerum yet)");
-        console.log(obj);
-        let result = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).insertOne(obj);
+        const id = client.
+        let obj = req.body
+        obj["_id"] = id
+        obj["@id"] = "https://storedev.rerum.io/v1/id/"+id
+        console.log("Creating an object (no history or __rerum yet)")
+        console.log(obj)
+        let result = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).insertOne(obj)
         res.set("Location", obj["@id"])
-        res.json(obj);
+        res.json(obj)
     }
     catch(err){
-        console.error("Could not perform insertOne, see error below");
+        console.error("Could not perform insertOne, see error below")
         console.log(err)
-        res.json({"err":err});
+        res.json({"err":err})
     }
-};
+}
 
 // Create object passed in the body
 // TODO only registered apps should be able to do this.  It should alter history.
 // Note this is the same thing as an /overwrite without history when you don't care about __rerum.generatedBy.
 exports.putUpdate = async function (req, res) {
     
-};
+}
 
 // Overwrite object passed in the body with replaceOne 
 // TODO only registered apps, and only if the requestor is of the agent __rerum.generatedBy for the object being overwritten.
 exports.overwrite = async function (req, res) {
-    res.set("Content-Type", "application/json; charset=utf-8");
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "*");
-    res.set("Access-Control-Expose-Headers", "*");
-    res.set("Access-Control-Allow-Methods", "*");
+    res.set("Content-Type", "application/json charset=utf-8")
+    res.set("Access-Control-Allow-Origin", "*")
+    res.set("Access-Control-Allow-Headers", "*")
+    res.set("Access-Control-Expose-Headers", "*")
+    res.set("Access-Control-Allow-Methods", "*")
     try{
-        let obj = req.body;
+        let obj = req.body
         if(obj.hasOwnProperty("@id")){
-            console.log("Overwriting an object (no history or __rerum yet)");
-            const query = {"@id":obj["@id"]};
-            let result = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).replaceOne(query, obj);
+            console.log("Overwriting an object (no history or __rerum yet)")
+            const query = {"@id":obj["@id"]}
+            let result = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).replaceOne(query, obj)
             if(result.modifiedCount > 0){
                 res.set("Location", obj["@id"])
-                res.json(obj);
+                res.json(obj)
             }
             else{
-                res.sendStatus(304);
+                res.sendStatus(304)
             }
         }    
         else{
@@ -78,115 +74,64 @@ exports.overwrite = async function (req, res) {
         } 
     }
     catch(err){
-        console.error("Could not perform overwrite, see error below");
+        console.error("Could not perform overwrite, see error below")
         console.log(err)
-        res.json({"err":err});
+        res.json({"err":err})
     }
-};
+}
 
 // Handle find by property object matching
 exports.query = async function (req, res) {
-    res.set("Content-Type", "application/json; charset=utf-8");
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "*");
-    res.set("Access-Control-Expose-Headers", "*");
-    res.set("Access-Control-Allow-Methods", "*");
+    res.set("Content-Type", "application/json charset=utf-8")
+    res.set("Access-Control-Allow-Origin", "*")
+    res.set("Access-Control-Allow-Headers", "*")
+    res.set("Access-Control-Expose-Headers", "*")
+    res.set("Access-Control-Allow-Methods", "*")
     try{
         let props = req.body
-        console.log("Looking matches against props...");
-        console.log(props);
-        let matches = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).find(props).toArray();
-        console.log(matches);
-        res.json(matches);
+        console.log("Looking matches against props...")
+        console.log(props)
+        let matches = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).find(props).toArray()
+        console.log(matches)
+        res.json(matches)
     }
     catch(err){
-        console.error("Could not perform query, see error below");
+        console.error("Could not perform query, see error below")
         console.log(err)
-        res.json([]);
+        res.json([])
     }
-};
-
-// Just makes a very simple object based on a simple model.  Happens on the fly.
-exports.makeNew = function (req, res) {
-    var modelObj = new Model();
-    const id = new mongoose.Types.ObjectId();
-    const RERUM_PREFIX = "https://storedev.rerum.io/v1/id/";
-    modelObj["_id"] = id;
-    modelObj["@id"] = RERUM_PREFIX+id;
-    modelObj.name = "Hello "+Date.now();
-    // save the contact and check for errors
-    //Here, we can hand in an object that is not formed from the model
-    console.log("Use Model.create to put the following object into the db");
-    console.log(modelObj);
-    Model.create(modelObj, function (err, data) {
-        if (err){
-            console.log("Error with Model.create");
-            console.error(err);
-            res.send(err);
-        }
-        else{
-            console.log("Success with Model.create");
-            res.json(data); //hmm not sure this is right, maybe just do modelObj?
-        }
-    });
-};
-
-
-// Handle find by property object matching
-exports.getByProps = function (req, res) {
-    res.set("Content-Type", "application/json; charset=utf-8");
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "*");
-    res.set("Access-Control-Expose-Headers", "*");
-    res.set("Access-Control-Allow-Methods", "*");
-    let prop = req.body
-    console.log("Props request object");
-    console.log(prop);
-    Model.find(prop, function (err, obj) {
-        if (err){
-            console.log("Model.findOne did not work as expected")
-            console.error(err)
-            res.send(err);
-        }
-        else{
-            console.log("View found object");
-            console.log(obj)
-            res.json(obj);
-        }
-    });
-};
+}
 
 //  Find by _id and return the match
 exports.id = async function (req, res) {
-    res.set("Content-Type", "application/json; charset=utf-8");
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "*");
-    res.set("Access-Control-Expose-Headers", "*");
-    res.set("Access-Control-Allow-Methods", "*");
-    let id = req.params["_id"];
-    let match = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).findOne({"_id" : id});
+    res.set("Content-Type", "application/json charset=utf-8")
+    res.set("Access-Control-Allow-Origin", "*")
+    res.set("Access-Control-Allow-Headers", "*")
+    res.set("Access-Control-Expose-Headers", "*")
+    res.set("Access-Control-Allow-Methods", "*")
+    let id = req.params["_id"]
+    let match = await client.db(process.env.MONGODBNAME).collection(process.env.MONGODBCOLLECTION).findOne({"_id" : id})
     if(match){
-        res.json(match);    
+        res.json(match)    
     }
     else{
-        res.sendStatus(404);
+        res.sendStatus(404)
     }  
-};
+}
 
-/*
-// Handle find by _id with Model
-exports.id = function (req, res) {
-    let id =  req.params["_id"]
-    Model.findById(id, function (err, obj) {
-        if (err){
-            console.error("")
-            res.send(err);
-        }
-        else{
-            console.log("View found object");
-            console.log(obj)
-            res.json(obj);
-        }
-    });
-};
-*/
+//Connect to a mongodb via mongodb node driver.
+async function mongoConnection(){
+  console.log("Awaiting mongo connection...")
+  try {
+      const client = new MongoClient(process.env.MONGO_CONNECTION_STRING)
+      let clientConnection = await client.connect()
+      console.log('Connected successfully to mongodb client')
+      //const db = client.db(dbName)
+      //const collection = db.collection('documents')
+      return clientConnection
+  } 
+  catch (err) {
+    console.log('mongo connect error in app initializer: ')
+    return err
+  } 
+}
