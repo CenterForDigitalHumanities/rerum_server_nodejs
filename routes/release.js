d726bdee3f1f29c3229343789a3d74b6c9ab06a6
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-04-07 09:19:46 -0500

    add releases route to git

---
+++ routes/release.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const router = require('express').Router()
+//This controller will handle all MongoDB interactions.
+const controller = require('../db-controller.js')
+const auth = require('../auth')
+
+//TODO also support getting the ?slug= varriable and detecting the Slug header to mint a custom ID to release the object with.
+router.route('/:_id')
+    .put(auth.checkJwt, controller.putUpdate)
+    .all((req, res) => {
+        res.statusMessage = 'Improper request method for releasing, please use PATCH to release this object.'
+        res.status(405)
+        next()
+    })
+
+module.exports = router
