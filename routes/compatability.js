8114e0bfd72c223e54ce0d528d8f444f4e6f9c6c
cubap <cubap@slu.edu>
2022-09-01 10:56:39 -0500

    carry over querystrings and paths

--- routes/compatability.js
+++ routes/compatability.js
@@ -4,7 +4,7 @@ const rewrite = require('express-urlrewrite')
 const auth = require('../auth')
 // This controller reroutes older style API calls.
 
-router.use(rewrite("/:attemptedAction.action", "/:attemptedAction"))
+router.use(rewrite("/:attemptedAction.action*", "/:attemptedAction$2"))
 router.use(rewrite("/getByProperties", "/query"))
 router.post('/accessToken',auth.generateNewAccessToken)
 router.post('/refreshToken',auth.generateNewRefreshToken)
