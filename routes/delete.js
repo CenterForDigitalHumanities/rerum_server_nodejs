fd6121c7341da29097cdc2d4a5b4b95c1d6b42ea
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-03-30 19:50:17 -0500

    only deploy on dev.  Try this.

--- routes/delete.js
+++ routes/delete.js
@@ -3,15 +3,6 @@ const router = require('express').Router()
 const controller = require('../db-controller.js')
 const auth = require('../auth')
 
-/*
-router.route('/:_id')
-    .delete(auth.checkJwt, controller.delete)
-    .all((req, res, next) => {
-        res.statusMessage = 'Improper request method for deleting, please use DELETE.'
-        res.status(405)
-        next(res)
-    })
-
 router.route('/')
     .delete(auth.checkJwt, controller.delete)
     .all((req, res, next) => {
@@ -19,24 +10,5 @@ router.route('/')
         res.status(405)
         next(res)
     })
-*/
-
-router
-    .delete("/", auth.checkJwt, controller.delete)
-    .all((req, res, next) => {
-        res.statusMessage = 'Improper request method for deleting, please use DELETE.'
-        res.status(405)
-        next(res)
-    })
-    
-router
-    .delete("/:_id", auth.checkJwt, controller.delete)
-    .all((req, res, next) => {
-        res.statusMessage = 'Improper request method for deleting, please use DELETE.'
-        res.status(405)
-        next(res)
-    })
-
-
 
 module.exports = router
