3b3e2b00fcbd8afaa01f91be8470df52d67cf79b
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-03-30 19:47:23 -0500

    oof.  try this.

--- routes/delete.js
+++ routes/delete.js
@@ -20,13 +20,6 @@ router.route('/')
         next(res)
     })
 */
-router
-    .delete("/:_id", auth.checkJwt, controller.delete)
-    .all((req, res, next) => {
-        res.statusMessage = 'Improper request method for deleting, please use DELETE.'
-        res.status(405)
-        next(res)
-    })
 
 router
     .delete("/", auth.checkJwt, controller.delete)
@@ -35,5 +28,15 @@ router
         res.status(405)
         next(res)
     })
+    
+router
+    .delete("/:_id", auth.checkJwt, controller.delete)
+    .all((req, res, next) => {
+        res.statusMessage = 'Improper request method for deleting, please use DELETE.'
+        res.status(405)
+        next(res)
+    })
+
+
 
 module.exports = router
