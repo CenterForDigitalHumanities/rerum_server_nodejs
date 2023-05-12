c1f13a6b230eb0eb5753b948f6cd843b518d9fef
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-05-12 15:13:54 -0500

    hmm change while testing

--- routes/compatability.js
+++ routes/compatability.js
@@ -6,7 +6,7 @@ const auth = require('../auth')
 
 router.use(rewrite("/:attemptedAction.action*", "/:attemptedAction$2"))
 router.use(rewrite("/getByProperties*", "/query$1"))
-router.use(rewrite("/batch_create*", "/bulkCreate$1"))
+router.use(rewrite("/batch_create*", "/bulkCreate$3"))
 router.post('/accessToken',auth.generateNewAccessToken)
 router.post('/refreshToken',auth.generateNewRefreshToken)
 
