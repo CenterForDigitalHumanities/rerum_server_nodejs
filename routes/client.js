813993a8197ea030a3034e7e010eb81634f9b25c
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-05-17 14:14:38 -0500

    authorize endpoint syntax

--- routes/client.js
+++ routes/client.js
@@ -6,11 +6,9 @@ router.get('/register', function(req,res,next){
   //Register means register with the RERUM Server Auth0 client and get a new code for a refresh token.
   //See https://auth0.com/docs/libraries/custom-signup
       var params = new URLSearchParams({
-          // "audience":process.env.AUDIENCE,
-          // "scope":"openid name email offline_access",
-          //"scope":"name email openid offline_access",
+          "audience":process.env.AUDIENCE,
+          "scope":"openid name email offline_access",
           "response_type":"code",
-          //"response_type":"token",
           "client_id":process.env.CLIENT_ID,
           "redirect_uri":process.env.RERUM_PREFIX,
           "state":"register"           
