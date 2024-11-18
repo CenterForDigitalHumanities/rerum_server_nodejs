65af53f68a916b92bcad5f31cb90347153bb1676
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-11-18 16:01:13 -0600

    ready

--- jest.config.js
+++ jest.config.js
@@ -205,7 +205,7 @@ const config = {
   // unmockedModulePathPatterns: undefined,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  watchPathIgnorePatterns: ['globalConfig'],
+  // watchPathIgnorePatterns: ['globalConfig'],
 
   // Whether to use watchman for file crawling
   // watchman: true,
