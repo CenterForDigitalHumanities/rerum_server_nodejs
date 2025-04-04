1e65081b88c1a91d62c318814340a28f03a7d0ee
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-04-04 09:30:07 -0500

    can put this back now

--- routes/bulkCreate.js
+++ routes/bulkCreate.js
@@ -11,7 +11,7 @@ router.route('/')
     .all((req, res, next) => {
         res.statusMessage = 'Improper request method for creating, please use POST.'
         res.status(405)
-        next()
+        next(res)
     })
 
 export default router
