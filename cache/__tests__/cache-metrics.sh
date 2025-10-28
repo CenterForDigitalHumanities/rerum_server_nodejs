760a53f599733cd5a000900be802400776d0e7be
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-10-28 10:48:33 -0500

    Point to devstore

--- cache/__tests__/cache-metrics.sh
+++ cache/__tests__/cache-metrics.sh
@@ -18,7 +18,7 @@
 # set -e
 
 # Configuration
-BASE_URL="${BASE_URL:-http://localhost:3001}"
+BASE_URL="${BASE_URL:-https://devstore.rerum.io}"
 API_BASE="${BASE_URL}/v1"
 # Auth token will be prompted from user
 AUTH_TOKEN=""
