b48e6f94df672b1023288cdf6961d8380fb95117
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2026-03-26 11:08:44 -0500

    hotfix because this should be 401 not 400

--- rest.js
+++ rest.js
@@ -142,8 +142,7 @@ const messenger = function (err, req, res, next) {
     }
     let token = req.header("Authorization")
     if(token && !token.startsWith("Bearer ")){
-        error.message +=`
-        Your token is not in the correct format.  It should be a Bearer token formatted like: "Bearer <token>"`
+        error.message +=`Your token is not in the correct format.  It should be a Bearer token formatted like: "Bearer <token>"`
         error.status = 401
     }
     switch (error.status) {
